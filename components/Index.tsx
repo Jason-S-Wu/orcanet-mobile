7fabd71ccbf29ee989454d788c82957ddb0566b7
JackNi49 <Jackni0241@gmail.com>
2024-03-03 20:31:28 -0500

    refactor the boilerplate code into basic layout desgin for app

---
+++ components/Index.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import ViewerTab from "./Tabs/ViewerTab";
+import SearchTab from "./Tabs/SearchTab";
+import StatsTab from "./Tabs/StatsTab";
+import SettingTab from "./Tabs/SettingTab";
+import { Feather, FontAwesome, Ionicons } from "@expo/vector-icons";
+
+const Index = () => {
+  const [activeTab, setActiveTab] = useState("Viewer");
+
+  const renderScreen = () => {
+    switch (activeTab) {
+      case "Viewer":
+        return <ViewerTab />;
+      case "Search":
+        return <SearchTab />;
+      case "Stats":
+        return <StatsTab />;
+      case "Setting":
+        return <SettingTab />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <TouchableOpacity style={styles.topBarButton}>
+          <Ionicons name="settings" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.content}>{renderScreen()}</View>
+      <View style={styles.bottomBar}>
+        <TouchableOpacity
+          onPress={() => setActiveTab("Viewer")}
+          style={styles.tab}
+        >
+          <FontAwesome name="play" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setActiveTab("Search")}
+          style={styles.tab}
+        >
+          <Feather name="file" size={24} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setActiveTab("Stats")}
+          style={styles.tab}
+        >
+          <Feather name="bar-chart" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    padding: 30,
+    width: "100%",
+  },
+  topBarButton: {
+    paddingHorizontal: 20,
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+  },
+  bottomBar: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#eee",
+    paddingVertical: 10,
+    width: "100%",
+  },
+  tab: {
+    paddingHorizontal: 20,
+  },
+});
+
+export default Index;
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import { Header } from '@rneui/base';
-import { View } from 'react-native';
-import Menu from './Menu';
-import Home from './Home';
+import React, { useState } from "react";
+import { Header } from "@rneui/base";
+import { View } from "react-native";
+import Menu from "./Menu";
+import Home from "./Index";
 
-export default () => {
-  const [menu, setMenu] = React.useState('Home');
+const Main = () => {
+  const [menu, setMenu] = useState("Home");
 
   const handleMenuClick = (menuItem: string) => {
     setMenu(menuItem);
@@ -13,9 +13,9 @@ export default () => {
 
   const renderPage = () => {
     switch (menu) {
-      case 'Home':
+      case "Home":
         return <Home />;
-      case 'Menu':
+      case "Menu":
         return <Menu />;
       default:
         return null;
@@ -28,12 +28,24 @@ export default () => {
         barStyle="default"
         centerComponent={{
           text: menu,
-          style: { color: '#fff', fontSize: 25 },
+          style: { color: "#fff", fontSize: 25 },
         }}
-        leftComponent={{ icon: 'menu', color: '#fff', onPress: () => handleMenuClick('Menu'), size: 30 }}
-        rightComponent={{ icon: 'home', color: '#fff', onPress: () => handleMenuClick('Home'), size: 30 }}
+        leftComponent={{
+          icon: "menu",
+          color: "#fff",
+          onPress: () => handleMenuClick("Menu"),
+          size: 60,
+        }}
+        rightComponent={{
+          icon: "home",
+          color: "#fff",
+          onPress: () => handleMenuClick("Home"),
+          size: 60,
+        }}
       />
       {renderPage()}
     </View>
   );
 };
+
+export default Main;
